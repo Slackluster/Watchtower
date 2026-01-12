aedb36222f7b4099803cd56482c73cb07505987f
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2026-01-12 11:37:41 +0300

    Update ruRU.lua

--- locales/ruRU.lua
+++ locales/ruRU.lua
@@ -9,7 +9,7 @@ local appName, app = ...
 local L = app.locales
 
 -- Settings
--- L.SETTINGS_VERSION =					GAME_VERSION_LABEL .. ":"	-- "Version"
+L.SETTINGS_VERSION =					GAME_VERSION_LABEL .. ":"	-- "Version"
 L.SETTINGS_SUPPORT_TEXTLONG =			"Разработка этого аддона требует значительного времени и усилий.\nПожалуйста, рассмотрите возможность финансовой поддержки разработчика."
 L.SETTINGS_SUPPORT_TEXT =				"Поддержать"
 L.SETTINGS_SUPPORT_BUTTON =				"Buy Me a Coffee" -- Brand name, if there isn't a localised version, keep it the way it is
@@ -24,17 +24,17 @@ L.SETTINGS_URL_COPY =					"Ctrl+C — скопировать:"
 L.SETTINGS_URL_COPIED =					"Ссылка скопирована в буфер обмена"
 
 L.SETTINGS_KEYSLASH_TITLE =				SETTINGS_KEYBINDINGS_LABEL .. " & Слэш-команды"	-- "Keybindings"
--- _G["BINDING_NAME_WATCHTOWER_TOGGLE"] =	"Toggle Edit Panel"
--- L.SLASH_TOGGLE_EDITPANEL =				"Toggle the edit panel"
--- L.SLASH_OPEN_SETTINGS =					"Откройте настройки"
+_G["BINDING_NAME_WATCHTOWER_TOGGLE"] =	"Включить панель редактирования"
+L.SLASH_TOGGLE_EDITPANEL =				"Включить панель редактирования"
+L.SLASH_OPEN_SETTINGS =					"Откройте настройки"
 
--- L.GENERAL =								GENERAL	-- "General"
+L.GENERAL =								GENERAL	-- "General"
 
 -- General
 L.NEW_VERSION_AVAILABLE =				"Доступна новая версия " .. app.NameLong .. ":"
--- L.SETTINGS_TOOLTIP =					app.NameLong .. "\n|cffFFFFFF"
--- 										.. app:ShowIcon(app.IconLMB) .. ": " .. L.SLASH_TOGGLE_EDITPANEL .. "\n"
--- 										.. app:ShowIcon(app.IconRMB) .. ": " .. L.SLASH_OPEN_SETTINGS
+L.SETTINGS_TOOLTIP =					app.NameLong .. "\n|cffFFFFFF"
+ 										.. app:ShowIcon(app.IconLMB) .. ": " .. L.SLASH_TOGGLE_EDITPANEL .. "\n"
+ 										.. app:ShowIcon(app.IconRMB) .. ": " .. L.SLASH_OPEN_SETTINGS
 
 L.INVALID_COMMAND =						"Неверная команда."
--- L.OR =									"or"
+L.OR =									"или"
