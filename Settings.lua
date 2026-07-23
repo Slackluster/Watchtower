e7a43ff9cf1a0c01e0f74841aeb8025992523750
Slackluster <29845810+Slackluster@users.noreply.github.com>
2026-07-23 23:34:04 +0200

    Combat feedback

--- Settings.lua
+++ Settings.lua
@@ -46,7 +46,11 @@ end)
 --------------
 
 function app:OpenSettings()
-	Settings.OpenToCategory(app.SettingsCategory:GetID())
+	if InCombatLockdown() then
+		app:Print(ERR_AFFECTING_COMBAT..".")
+	else
+		Settings.OpenToCategory(app.SettingsCategory:GetID())
+	end
 end
 
 function app:CreateMinimapButton()
