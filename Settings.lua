4eafacd78565291f3feccb4b7f4a21f6d0d2ca4a
Slackluster <29845810+Slackluster@users.noreply.github.com>
2026-09-16 15:23:29 +0200

    Update New setting visual

--- Settings.lua
+++ Settings.lua
@@ -192,11 +192,27 @@ function app:CreateSettings()
 
 	local category, layout
 
+	local function addNewTag(initializer, variable, setting1, setting2)
+		if NewSettings and not app.Settings.seenNew[variable] then
+			local _, _, _, interfaceVersion = GetBuildInfo()
+			local patch = string.format("%d.%d.%d", math.floor(interfaceVersion / 10000), math.floor(interfaceVersion / 100) % 100, interfaceVersion % 100)
+
+			initializer.data.newTagID = appName .. "_" .. variable
+
+			NewSettings[patch] = NewSettings[patch] or {}
+			table.insert(NewSettings[patch], appName .. "_" .. variable)
+
+			local function callback() app.Settings.seenNew[variable] = true end
+			if setting1 then setting1:SetValueChangedCallback(callback) end
+			if setting2 then setting2:SetValueChangedCallback(callback) end
+		end
+	end
+
 	local function button(name, buttonName, description, func)
 		layout:AddInitializer(CreateSettingsButtonInitializer(name, buttonName, func, description, true))
 	end
 
-	local function checkbox(variable, name, description, default, callback, parentSetting, parentCheckbox)
+	local function checkbox(variable, name, description, default, callback, parentSetting, parentCheckbox, isNew)
 		local setting = Settings.RegisterAddOnSetting(category, appName .. "_" .. variable, variable, app.Settings, type(default), name, default)
 		local checkbox = Settings.CreateCheckbox(category, setting, description)
 
@@ -209,12 +225,14 @@ function app:CreateSettings()
 			setting:SetValueChangedCallback(callback)
 		end
 
+		if isNew then addNewTag(checkbox, variable, setting) end
+
 		return setting, checkbox
 	end
 
-	local function checkboxDropdown(cbVariable, cbName, description, cbDefaultValue, ddVariable, ddDefaultValue, options, callback)
-		local cbSetting = Settings.RegisterAddOnSetting(category, appName.."_"..cbVariable, cbVariable, app.Settings, type(cbDefaultValue), cbName, cbDefaultValue)
-		local ddSetting = Settings.RegisterAddOnSetting(category, appName.."_"..ddVariable, ddVariable, app.Settings, type(ddDefaultValue), "", ddDefaultValue)
+	local function checkboxDropdown(cbVariable, cbName, description, cbDefaultValue, ddVariable, ddDefaultValue, options, callback, isNew)
+		local cbSetting = Settings.RegisterAddOnSetting(category, appName .. "_" .. cbVariable, cbVariable, app.Settings, type(cbDefaultValue), cbName, cbDefaultValue)
+		local ddSetting = Settings.RegisterAddOnSetting(category, appName .. "_" .. ddVariable, ddVariable, app.Settings, type(ddDefaultValue), "", ddDefaultValue)
 		local function GetOptions()
 			local container = Settings.CreateControlTextContainer()
 			for _, option in ipairs(options) do
@@ -230,10 +248,12 @@ function app:CreateSettings()
 			cbSetting:SetValueChangedCallback(callback)
 			ddSetting:SetValueChangedCallback(callback)
 		end
+
+		if isNew then addNewTag(initializer, cbVariable, cbSetting, ddSetting) end
 	end
 
-	local function dropdown(variable, name, description, default, options, callback)
-		local setting = Settings.RegisterAddOnSetting(category, appName.."_"..variable, variable, app.Settings, type(default), name, default)
+	local function dropdown(variable, name, description, default, options, callback, isNew)
+		local setting = Settings.RegisterAddOnSetting(category, appName .. "_" .. variable, variable, app.Settings, type(default), name, default)
 		local function GetOptions()
 			local container = Settings.CreateControlTextContainer()
 			for _, option in ipairs(options) do
@@ -241,10 +261,14 @@ function app:CreateSettings()
 			end
 			return container:GetData()
 		end
-		Settings.CreateDropdown(category, setting, GetOptions, description)
+
+		local initializer = Settings.CreateDropdown(category, setting, GetOptions, description)
+
 		if callback then
 			setting:SetValueChangedCallback(callback)
 		end
+
+		if isNew then addNewTag(initializer, variable, setting) end
 	end
 
 	local function expandableHeader(name)
